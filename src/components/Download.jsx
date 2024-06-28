ee26e65317304249132890af4756ec71b32548ce
Septian Adi Pratama <septianadip7998@gmail.com>
2024-06-28 21:49:16 +0700

    clean code

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -6,52 +6,46 @@ import illustrationMockup from "../assets/images/illustration-mockup.png";
 const Download = () => {
   return (
     <div className="container mx-auto p-6 rounded-lg flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6 bg-blue-400">
+      
       {/* Left Content */}
       <div className="flex-1">
-        <div className="container">
-          {/* Tag Start Now */}
-          <div className="inline-flex items-center mb-4 justify-start border-2 border-white rounded-full p-2">
-            <img
-              src={heartIcon}
-              alt="heart icon"
-              className="w-6 h-6 text-blue-500"
-            />
-            <span className="px-2 rounded-full font-semibold text-white">
-              Mulai Sekarang
-            </span>
-          </div>
-          {/* Title and description */}
-          <h1 className="text-3xl md:text-3xl font-semibold mb-2 text-white">
-            Kelola bisnis laundry anda <br /> dengan segala kemudahan <br />{" "}
-            dengan POS Laundry
-          </h1>
-          <p className="font-medium mb-4 text-white">
-            POS Laundry by LaundryAku.com
-          </p>
-
-          {/* Buttons store */}
-          <div className="flex space-x-4 ">
-            <a
-              href="#"
-              className="bg-white text-black py-2 px-4 rounded-full flex items-center space-x-2 hover:bg-blue-500 hover:text-white"
-            >
-              <img src={playstoreIcon} alt="Playstore" className="w-8 h-8" />
-              <span className="font-semibold">Playstore</span>
-            </a>
-            <a
-              href="#"
-              className="bg-white text-black py-2 px-4 rounded-full flex items-center space-x-2 hover:bg-blue-500 hover:text-white"
-            >
-              <img src={appstoreIcon} alt="App Store" className="w-8 h-8" />
-              <span className="font-semibold">App Store</span>
-            </a>
-          </div>
+        <div className="inline-flex items-center mb-4 border-2 border-white rounded-full p-2">
+          <img
+            src={heartIcon}
+            alt="heart icon"
+            className="w-6 h-6 text-blue-500"
+          />
+          <span className="px-2 rounded-full font-semibold text-white">
+            Mulai Sekarang
+          </span>
+        </div>
+        <h1 className="text-4xl md:text-3xl font-semibold mb-2 text-white">
+          Kelola bisnis laundry anda <br /> dengan segala kemudahan <br />{" "}
+          dengan POS Laundry
+        </h1>
+        <p className="font-medium mb-4 text-white">
+          POS Laundry by LaundryAku.com
+        </p>
+        <div className="flex space-x-4">
+          <a
+            href="#"
+            className="bg-white text-black py-2 px-4 rounded-full flex items-center space-x-2 hover:bg-blue-500 hover:text-white"
+          >
+            <img src={playstoreIcon} alt="Playstore" className="w-8 h-8" />
+            <span className="font-semibold">Playstore</span>
+          </a>
+          <a
+            href="#"
+            className="bg-white text-black py-2 px-4 rounded-full flex items-center space-x-2 hover:bg-blue-500 hover:text-white"
+          >
+            <img src={appstoreIcon} alt="App Store" className="w-8 h-8" />
+            <span className="font-semibold">App Store</span>
+          </a>
         </div>
       </div>
 
       {/* Right Content */}
-      <div className="flex-1 flex justify-center">
-        {/* Illustration mockup */}
+      <div className="flex-1 flex justify-end">
         <img
           src={illustrationMockup}
           alt="Mobile App Screenshot"
