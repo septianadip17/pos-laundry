6247a74c23adeac67cd7393b80ff8d32499a5006
Septian Adi Pratama <septianadip7998@gmail.com>
2024-06-30 01:24:01 +0700

    link section center-text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,58 +47,60 @@ const socialLinks = [
 
 const Footer = () => {
   return (
-    <div className="container mx-auto py-10 px-4 md:px-10">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-12">
-        {/* Logo and Social Media */}
-        <div className="md:col-span-1 mb-4">
-          <div className="flex items-center space-x-1">
-            <img src={logo} alt="Laundry logo" className="w-6 h-6" />
-            <span className="text-2xl font-bold">Laundry</span>
+    <>
+      <div className="container mx-auto py-10 px-4 md:px-10">
+        <div className="flex flex-col md:flex-row">
+          {/* Logo and Social Media */}
+          <div className="flex-1 mb-10 md:mb-0">
+            <div className="flex items-center space-x-1">
+              <img src={logo} alt="Laundry logo" className="w-6 h-6" />
+              <span className="text-2xl font-bold">Laundry</span>
+            </div>
+            <p className="mt-4 text-gray-600">
+              Aplikasi POS Laundry untuk <br /> membantu owner laundry
+            </p>
+            <div className="flex space-x-4 mt-2">
+              {socialLinks.map((link, index) => (
+                <a
+                  key={index}
+                  href={link.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-transform transform hover:scale-110"
+                >
+                  <img src={link.src} alt={link.alt} className="w-6 h-6" />
+                </a>
+              ))}
+            </div>
           </div>
-          <p className="mt-4 text-gray-600">
-            Aplikasi POS Laundry untuk <br /> membantu owner laundry
-          </p>
-          <div className="flex space-x-4 mt-2">
-            {socialLinks.map((link, index) => (
-              <a
-                key={index}
-                href={link.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="transition-transform transform hover:scale-110"
-              >
-                <img src={link.src} alt={link.alt} className="w-6 h-6" />
-              </a>
+
+          {/* Links Section */}
+          <div className="flex-2 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-5">
+            {Object.entries(links).map(([key, section]) => (
+              <div key={key}>
+                <h3 className="font-semibold text-center">{section.title}</h3>
+                <ul className="mt-1 space-y-2">
+                  {section.items.map((item, index) => (
+                    <li key={index} className="hover:underline text-center text-gray-600">
+                      <a
+                        href={item.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             ))}
           </div>
         </div>
-
-        {/* Links Section */}
-        <div className="md:col-span-2 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-5">
-          {Object.entries(links).map(([key, section]) => (
-            <div key={key}>
-              <h3 className="font-semibold">{section.title}</h3>
-              <ul className="mt-1 space-y-2">
-                {section.items.map((item, index) => (
-                  <li key={index} className="hover:underline text-gray-600">
-                    <a
-                      href={item.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
       </div>
-      <div className="text-center mt-6 text-gray-600">
+      <div className="text-center mt-6 text-gray-600 mb-10">
         © Copyright POS Laundry 2024. All Rights Reserved
       </div>
-    </div>
+    </>
   );
 };
 
