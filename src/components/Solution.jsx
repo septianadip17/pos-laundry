b714330a6779b05a6ff53c0caf08da88c1fad455
Septian Adi Pratama <septianadip7998@gmail.com>
2024-06-29 09:44:15 +0700

    top section solution

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -4,29 +4,28 @@ import speaker from "../assets/icons/speaker.svg";
 const Solution = () => {
   return (
     <div className="flex flex-col items-center p-6 mb-4" id="solusi">
-      {/* Tag solution */}
-      <div className="flex items-center mb-4 border-2 border-blue-500 rounded-full p-2">
-        <img
-          src={speaker}
-          alt="speaker icon"
-          className="w-6 h-6 text-blue-500"
-        />
-        <span className="px-2 rounded-full font-semibold text-blue-500">
-          Solusi
-        </span>
-      </div>
-
-      {/* Title and description */}
-      <div className="container">
-        <h1 className="text-4xl font-bold mb-2 text-center">
-          Solusi inovatif pebisnis laundry <br className="hidden md:inline" />{" "}
-          mengembangkan bisnisnya
-        </h1>
-        <p className="text-gray-500 text-center mb-8">
-          Semua yang Anda butuhkan untuk menjalankan bisnis laundry Anda{" "}
-          <br className="hidden md:inline" /> dengan efisien dan efektif kini
-          ada dalam satu aplikasi.
-        </p>
+      <div className="container w-1/2 text-center">
+        {/* Tag solution */}
+        <div className="inline-flex items-center mb-4 border-2 border-blue-500 rounded-full p-2">
+          <img
+            src={speaker}
+            alt="speaker icon"
+            className="w-6 h-6 text-blue-500"
+          />
+          <span className="px-2 rounded-full font-semibold text-blue-500">
+            Solusi
+          </span>
+        </div>
+        {/* Title and description */}
+        <div className="container">
+          <h1 className="text-4xl font-bold mb-2 text-center">
+            Solusi inovatif pebisnis laundry mengembangkan bisnisnya
+          </h1>
+          <p className="text-gray-500 text-center mb-8">
+            Semua yang Anda butuhkan untuk menjalankan bisnis laundry Anda dengan efisien dan efektif kini
+            ada dalam satu aplikasi.
+          </p>
+        </div>
       </div>
 
       {/* Before after image */}
