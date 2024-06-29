c2a4c1f86948bf67fa89d89786af752a5bf0da4e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-06-29 10:49:52 +0700

    sollution fixing image

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -4,8 +4,8 @@ import speaker from "../assets/icons/speaker.svg";
 const Solution = () => {
   return (
     <div className="flex flex-col items-center p-6 mb-4" id="solusi">
-      <div className="container w-1/2 text-center">
-        {/* Tag solution */}
+      {/* Tag Solution */}
+      <div className="flex flex-col items-center w-full max-w-2xl text-center mb-8">
         <div className="inline-flex items-center mb-4 border-2 border-blue-500 rounded-full p-2">
           <img
             src={speaker}
@@ -17,23 +17,21 @@ const Solution = () => {
           </span>
         </div>
         {/* Title and description */}
-        <div className="container">
-          <h1 className="text-4xl font-bold mb-2 text-center">
-            Solusi inovatif pebisnis laundry mengembangkan bisnisnya
-          </h1>
-          <p className="text-gray-500 text-center mb-8">
-            Semua yang Anda butuhkan untuk menjalankan bisnis laundry Anda dengan efisien dan efektif kini
-            ada dalam satu aplikasi.
-          </p>
-        </div>
+        <h1 className="text-4xl font-bold mb-2">
+          Solusi inovatif pebisnis laundry mengembangkan bisnisnya
+        </h1>
+        <p className="text-gray-500 mb-8">
+          Semua yang Anda butuhkan untuk menjalankan bisnis laundry Anda dengan
+          efisien dan efektif kini ada dalam satu aplikasi.
+        </p>
       </div>
 
-      {/* Before after image */}
-      <div className="flex justify-center">
+      {/* Before After Image */}
+      <div className="w-full max-w-8xl flex justify-center">
         <img
           src={beforeAfter}
-          alt="Before image of laundry business"
-          className="rounded-lg shadow-lg mb-4"
+          alt="Before and after image of laundry business"
+          className="w-full"
         />
       </div>
     </div>
