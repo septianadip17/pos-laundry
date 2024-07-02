5d2d2b85d13e98e63d924fbf255f76edfaced0dc
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-02 16:45:17 +0700

    solution change the photo, make row photo on mobile

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,24 +1,27 @@
-import beforeAfter from "../assets/images/before-after.png";
+import before from "../assets/images/sebelum.png";
+import after from "../assets/images/sesudah.png";
 import speaker from "../assets/icons/speaker.svg";
 import Tag from "../props/Tag";
 
 const Solution = () => {
   return (
     <div className="flex flex-col items-center p-6 mb-4" id="solusi">
-      <Tag 
-        icon={speaker} 
-        alt="speaker icon" 
-        tag="Solusi" 
-        title="Solusi inovatif pebisnis laundry mengembangkan bisnisnya" 
+      {/* Tag Title */}
+      <Tag
+        icon={speaker}
+        alt="speaker icon"
+        tag="Solusi"
+        title="Solusi inovatif pebisnis laundry mengembangkan bisnisnya"
         desc="Semua yang Anda butuhkan untuk menjalankan bisnis laundry Anda dengan efisien dan efektif kini ada dalam satu aplikasi."
       />
       {/* Before After Image */}
-      <div className="w-full max-w-8xl flex justify-center">
-        <img
-          src={beforeAfter}
-          alt="Before and after image of laundry business"
-          className="w-full"
-        />
+      <div className="w-full max-w-8xl flex flex-col md:flex-row justify-center">
+        <div className="container flex-col">
+          <img src={before} alt="before" className="w-full h-auto" />
+        </div>
+        <div className="container flex-col">
+          <img src={after} alt="after" className="w-full h-auto" />
+        </div>
       </div>
     </div>
   );
