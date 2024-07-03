6d46d2ee5e5d3c2e6bb5d1e2a379aafe9fc823b3
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-03 11:01:10 +0700

    header clean code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,25 @@ import illustration from "../assets/images/illustration.png";
 const Header = () => {
   return (
     <div
+      className="container mx-auto bg-gray-100 flex flex-col lg:flex-row items-center px-4 lg:space-x-12 lg:space-y-0 space-y-2"
       id="home"
-      className="container bg-gray-100 mx-auto flex flex-col lg:flex-row items-center px-4 space-y-2 lg:space-y-0 lg:space-x-12"
     >
       {/* Left Content */}
       <div className="lg:w-1/2 space-y-6">
-        <h1 className="text-7xl lg:text-7xl font-semibold text-start space-x-2 lg:text-left">
+        {/* Main Title */}
+        <h1 className="text-7xl font-semibold text-start lg:text-left">
           Kelola Bisnis Laundry Anda dengan
-          <span className="inline-block px-4 py-1 bg-blue-500 text-white rounded-3xl">
+          <span className="inline-block px-2 py-1 bg-blue-500 text-white rounded-3xl">
             Mudah
           </span>
         </h1>
-        <p className="text-start text-lg lg:text-left text-gray-600 ">
+        {/* Description */}
+        <p className="text-lg text-start text-gray-600 lg:text-left">
           POS Laundry mempermudah Anda dalam mengelola order, keuangan, dan
           mengatur bisnis secara efisien. Semua yang Anda butuhkan dalam satu
           aplikasi praktis.
         </p>
-        {/* Download Buttons */}
-        <h1 className="text-xl text-start lg:text-left font-semibold">
+        <h1 className="text-xl font-semibold text-start lg:text-left">
           Download Aplikasi:
         </h1>
         <div className="flex justify-start lg:justify-start space-x-2">
@@ -33,6 +34,7 @@ const Header = () => {
             <img src={playstore} alt="Playstore" className="w-10 h-10" />
             <span className="font-medium text-2xl">Playstore</span>
           </a>
+          {/* Button Download Store */}
           <a
             href="#"
             className="bg-white text-black py-2 px-4 rounded-full flex items-center space-x-2 hover:bg-blue-500 hover:text-white shadow-xl"
@@ -43,14 +45,17 @@ const Header = () => {
         </div>
         {/* Statistics */}
         <div className="flex justify-between lg:justify-start space-x-5 mt-4">
+          {/* Downloads */}
           <div className="text-start">
             <p className="text-4xl font-semibold">25K</p>
             <p className="text-gray-700 text-xl">Download</p>
           </div>
+          {/* Partner */}
           <div className="text-start">
             <p className="text-4xl font-semibold">12K</p>
             <p className="text-gray-700 text-xl">Mitra laundry</p>
           </div>
+          {/* Rating */}
           <div className="text-start">
             <p className="text-4xl font-semibold">4.8</p>
             <p className="text-gray-700 text-xl">Rating playstore</p>
