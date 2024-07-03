cbb56849d12de226565ac2ec31497ab523e5e8d8
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-03 10:10:43 +0700

    fitur better layout

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import star from "../assets/icons/star.svg";
 import order from "../assets/images/order.png";
 import statistic from "../assets/images/statistic.png";
@@ -7,10 +8,87 @@ import analyticsReport from "../assets/images/analytic-and-report.png";
 import customerManagement from "../assets/images/customer-management.png";
 import Tag from "../props/Tag";
 
+const features = [
+  {
+    id: 1,
+    icon: order,
+    title: "Manajemen Pesanan",
+    description:
+      "Kelola semua pesanan laundry dengan mudah dan efisien. Dengan POS Laundry, Anda bisa melacak, mengatur, dan memproses pesanan dengan cepat, memastikan kepuasan pelanggan dan operasional yang lancar.",
+    image: orderManagement,
+    imageAlt: "order-management-screenshot",
+    colSpan: "md:col-span-2",
+    isOrderManagement: true,
+  },
+  {
+    id: 2,
+    icon: statistic,
+    title: "Analitik dan Laporan",
+    description:
+      "Dapatkan wawasan mendalam tentang kinerja bisnis laundry Anda dengan fitur analitik dan laporan kami. Pantau keuangan, identifikasi tren, dan buat keputusan yang lebih cerdas untuk mengoptimalkan bisnis Anda.",
+    image: analyticsReport,
+    imageAlt: "analytics-screenshot",
+    colSpan: "",
+  },
+  {
+    id: 3,
+    icon: customer,
+    title: "Manajemen Pelanggan",
+    description:
+      "Bangun hubungan yang lebih baik dengan pelanggan Anda. POS Laundry memudahkan Anda untuk mengelola informasi pelanggan, melacak riwayat pesanan, dan memberikan layanan yang lebih personal dan efektif.",
+    image: customerManagement,
+    imageAlt: "customer-management-screenshot",
+    colSpan: "",
+  },
+];
+
+const FeatureCard = ({
+  icon,
+  title,
+  description,
+  image,
+  imageAlt,
+  colSpan,
+  isOrderManagement,
+}) => (
+  <div
+    className={`col-span-1 flex flex-col bg-gray-100 p-6 rounded-lg shadow ${colSpan}`}
+  >
+    <div
+      className={`flex flex-col ${
+        isOrderManagement ? "md:flex-row" : "mb-4"
+      } md:items-start`}
+    >
+      <div className={`flex flex-col ${isOrderManagement ? "md:w-1/2" : ""}`}>
+        <img alt={`${title}-icon`} src={icon} className="w-12 h-12" /> {/* Icon */}
+        <h2 className="text-4xl my-3 font-semibold md:text-left">{title}</h2> {/* Title */}
+        <p className="text-gray-500 mb-6 text-justify">{description}</p> 
+      </div>
+      <div
+        className={`text-center flex justify-center ${
+          isOrderManagement ? "md:w-1/2 md:ml-4" : "md:w-full"
+        }`}
+      >
+        <img src={image} alt={imageAlt} className="w-96 rounded-lg shadow" /> {/* Image */}
+      </div>
+    </div>
+  </div>
+);
+
+FeatureCard.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
+  colSpan: PropTypes.string,
+  isOrderManagement: PropTypes.bool,
+};
+
 const Feature = () => {
   return (
     <div
-      className="container flex flex-col items-center mx-auto p-6"
+      className="container flex flex-col items-center mx-auto px-6"
       id="fitur"
     >
       <Tag
@@ -23,83 +101,9 @@ const Feature = () => {
 
       {/* Feature cards */}
       <div className="grid grid-cols-1 gap-6 max-w-7xl w-full md:grid-cols-2">
-        {/* Manajemen Pesanan */}
-        <div className="col-span-1 md:col-span-2 flex flex-col p-6 bg-gray-100 rounded-lg shadow">
-          <div className="flex flex-col md:flex-row md:items-start">
-            <img
-              alt="order-icon"
-              src={order}
-              className="w-12 h-12 md:order-first md:mb-4"
-            />
-            <div className="flex-1 md:text-left">
-              <h2 className="text-xl font-semibold mb-2">Manajemen Pesanan</h2>
-              <p className="mb-4 text-gray-500">
-                Kelola semua pesanan laundry dengan mudah dan efisien. Dengan
-                POS Laundry, Anda bisa melacak, mengatur, dan memproses pesanan
-                dengan cepat, memastikan kepuasan pelanggan dan operasional yang
-                lancar.
-              </p>
-            </div>
-            <div className="flex-shrink-0 md:ml-4 flex justify-center">
-              <img
-                src={orderManagement}
-                alt="order-management-screenshot"
-                className="w-96 rounded-lg shadow"
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* Analitik dan Laporan */}
-        <div className="col-span-1 flex flex-col bg-gray-100 p-6 rounded-lg shadow">
-          <div className="flex flex-col mb-4 md:items-start">
-            <img alt="analytics-icon" src={statistic} className="w-12 h-12" />
-          </div>
-          <div className="text-start">
-            <h2 className="text-xl font-semibold md:text-left">
-              Analitik dan Laporan
-            </h2>
-            <p className="text-gray-500 mb-6 text-justify">
-              Dapatkan wawasan mendalam tentang kinerja bisnis laundry Anda
-              dengan fitur analitik dan laporan kami. Pantau keuangan,
-              identifikasi tren, dan buat keputusan yang lebih cerdas untuk
-              mengoptimalkan bisnis Anda.
-            </p>
-          </div>
-          <div className="text-center flex justify-center">
-            <img
-              src={analyticsReport}
-              alt="analytics-screenshot"
-              className="w-96 rounded-lg shadow"
-            />
-          </div>
-        </div>
-
-        {/* Manajemen Pelanggan */}
-        <div className="col-span-1 flex flex-col bg-gray-100 p-6 rounded-lg shadow">
-          <div className="flex flex-col mb-4 md:items-start">
-            <img
-              alt="customer-management-icon"
-              src={customer}
-              className="w-12 h-12"
-            />
-          </div>
-          <h2 className="text-xl font-semibold md:text-left">
-            Manajemen Pelanggan
-          </h2>
-          <p className="text-gray-500 mb-6 text-justify">
-            Bangun hubungan yang lebih baik dengan pelanggan Anda. POS Laundry
-            memudahkan Anda untuk mengelola informasi pelanggan, melacak riwayat
-            pesanan, dan memberikan layanan yang lebih personal dan efektif.
-          </p>
-          <div className="text-center flex justify-center">
-            <img
-              src={customerManagement}
-              alt="customer-management-screenshot"
-              className="w-96 rounded-lg shadow"
-            />
-          </div>
-        </div>
+        {features.map((feature) => (
+          <FeatureCard key={feature.id} {...feature} />
+        ))}
       </div>
     </div>
   );
