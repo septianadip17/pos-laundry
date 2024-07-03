6949e7808f8ae7d44ba88e61c5f7d07d786436d0
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-03 12:35:36 +0700

    setting aos in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init({
+  // Global settings:
+  offset: 100, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
