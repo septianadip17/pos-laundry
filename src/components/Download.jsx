996839634ddd94d2946ca916b233768dda39e44b
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-16 23:47:09 +0700

    background size contain

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,10 +7,10 @@ import "aos/dist/aos.css";
 
 const styles = {
   container: {
-    width: "100%",
-    minHeight: "100%",
+    width: "90%",
+    minHeight: "50vh",
     borderRadius: "30px",
-    backgroundSize: "cover",
+    backgroundSize: "contain",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
   },
@@ -34,56 +34,50 @@ const Download = () => {
     : { ...styles.container, ...styles.containerDesktop };
 
   return (
-    // Download Section
-    <>
-      <div className="container mt-10">
-        <div
-          id="download"
-          className="container mx-auto px-4 rounded-lg flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6"
-          data-aos="slide-up"
-          style={containerStyle}
-        >
-          {/* Left Content */}
-          <div className="flex-1 md:text-left my-5">
-            <div className="inline-flex items-center mt-3 sm:my-5 border-2 bg-blue-400 border-white rounded-full p-2">
-              <img src={heartIcon} alt="heart icon" className="w-5 h-5" />
-              <span className="px-2 rounded-full font-normal text-white">
-                Mulai Sekarang
-              </span>
-            </div>
-            <h1 className="text-3xl font-semibold mb-6 mt-4 text-white leading-tight">
-              Kelola bisnis laundry anda dengan segala kemudahan dengan POS
-              Laundry
-            </h1>
-            <p className="font-normal text-lg mb-4 text-white">
-              POS Laundry by LaundryAku.com
-            </p>
-            {/* Buttons for App Stores */}
-            <div className="flex md:justify-start space-x-4">
-              <a
-                href="#"
-                className={`${styles.storeButton.base} ${styles.storeButton.hover}`}
-              >
-                <img src={playstoreIcon} alt="Playstore" className="w-8 h-8" />
-                <span className="font-semibold">Playstore</span>
-              </a>
-              <a
-                href="#"
-                className={`${styles.storeButton.base} ${styles.storeButton.hover}`}
-              >
-                <img src={appstoreIcon} alt="App Store" className="w-8 h-8" />
-                <span className="font-semibold">App Store</span>
-              </a>
-            </div>
-          </div>
-
-          {/* Right Content */}
-          <div className="flex-1 flex justify-center md:justify-end">
-            <div className="mb-96"></div>
-          </div>
+    <div
+      id="download"
+      className="container mx-auto px-6 rounded-lg flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6"
+      data-aos="slide-up"
+      style={containerStyle}
+    >
+      {/* Left Content */}
+      <div className="flex-1 md:text-left my-5">
+        <div className="inline-flex items-center mt-3 sm:my-5 border-2 bg-blue-400 border-white rounded-full p-2">
+          <img src={heartIcon} alt="heart icon" className="w-5 h-5" />
+          <span className="px-2 rounded-full font-normal text-white">
+            Mulai Sekarang
+          </span>
+        </div>
+        <h1 className="text-3xl font-semibold mb-6 mt-4 text-white leading-tight">
+          Kelola bisnis laundry anda dengan segala kemudahan dengan POS Laundry
+        </h1>
+        <p className="font-normal text-lg mb-4 text-white">
+          POS Laundry by LaundryAku.com
+        </p>
+        {/* Buttons for App Stores */}
+        <div className="flex md:justify-start space-x-4">
+          <a
+            href="#"
+            className={`${styles.storeButton.base} ${styles.storeButton.hover}`}
+          >
+            <img src={playstoreIcon} alt="Playstore" className="w-8 h-8" />
+            <span className="font-semibold">Playstore</span>
+          </a>
+          <a
+            href="#"
+            className={`${styles.storeButton.base} ${styles.storeButton.hover}`}
+          >
+            <img src={appstoreIcon} alt="App Store" className="w-8 h-8" />
+            <span className="font-semibold">App Store</span>
+          </a>
         </div>
       </div>
-    </>
+
+      {/* Right Content */}
+      <div className="flex-1 flex justify-center md:justify-end">
+        <div className="mb-96"></div>
+      </div>
+    </div>
   );
 };
 
